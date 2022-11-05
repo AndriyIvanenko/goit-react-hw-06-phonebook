a3619a150445cf8e1cd5cd2b45c90650ce98e55b
Andriy Ivanenko <andriy.ivanenko@gmail.com>
2022-11-05 16:58:00 +0200

    done

--- src/redux/contactsSlise.js
+++ src/redux/contactsSlise.js
@@ -1,20 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from 'nanoid';
 
-const contacts = [
+// const contacts = [
+const initialContacts = [
   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
 
-if (!localStorage.getItem('contacts')) {
-  localStorage.setItem('contacts', JSON.stringify(contacts));
-}
-
-const initialContacts = localStorage.getItem('contacts')
-  ? JSON.parse(localStorage.getItem('contacts'))
-  : [];
+// const initialContacts = localStorage.getItem('contacts')
+//   ? JSON.parse(localStorage.getItem('contacts'))
+//   : contacts;
 
 const contactsSlice = createSlice({
   name: 'contacts',
@@ -26,9 +23,10 @@ const contactsSlice = createSlice({
           alert(`${action.payload.name} is already in contacts`);
           return;
         }
-        const updatedContacts = [...state, action.payload];
-        localStorage.setItem('contacts', JSON.stringify(updatedContacts));
-        return updatedContacts;
+        // const updatedContacts = [...state, action.payload];
+        // localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+        // return updatedContacts;
+        return [...state, action.payload];
       },
       prepare(name, number) {
         return {
@@ -41,9 +39,10 @@ const contactsSlice = createSlice({
       },
     },
     deleteContact(state, action) {
-      const updatedContacts = state.filter(contact => contact.id !== action.payload);
-      localStorage.setItem('contacts', JSON.stringify(updatedContacts));
-      return updatedContacts;
+      // const updatedContacts = state.filter(contact => contact.id !== action.payload);
+      // localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+      // return updatedContacts;
+      return state.filter(contact => contact.id !== action.payload);
     },
   },
 });
